997b523c24ab4c7fced8b879f04f5704b670e132
wooorm <tituswormer@gmail.com>
2014-09-27 13:40:30 +0200

    Refactor API

--- index.js
+++ index.js
@@ -1,62 +1,106 @@
 'use strict';
 
-var franc = require('franc'),
-    visit = require('retext-visit');
+/**
+ * Dependencies.
+ */
+
+var franc,
+    visit;
+
+franc = require('franc');
+visit = require('retext-visit');
 
 /**
  * Deep regular sort on the number at `1` in both objects.
  *
- * @param {Array} a
- * @param {Array} b
- * @api private
+ * @example
+ *   > [[0, 20], [0, 1], [0, 5]].sort(sort);
+ *   // [[0, 1], [0, 5], [0, 20]]
+ *
+ * @param {{1: number}} a
+ * @param {{1: number}} b
  */
+
 function sort(a, b) {
     return a[1] - b[1];
 }
 
-function sortDistanceObject(distanceObject) {
-    var distances = [],
-        iterator = -1,
+/**
+ * Get a sorted array containing language--distance
+ * tuples from an object with trigrams as its
+ * attributes, and their occurence count as their
+ * values.
+ *
+ * @param {Object.<string, number>} dictionary
+ * @return {Array.<Array.<string, number>>} An
+ *   array containing language--distance tupples,
+ *   sorted by count (low to high).
+ */
+
+function sortDistanceObject(dictionary) {
+    var distances,
         distance;
 
-    for (distance in distanceObject) {
-        distances[++iterator] = [distance, distanceObject[distance]];
+    distances = [];
+
+    for (distance in dictionary) {
+        distances.push([distance, dictionary[distance]]);
     }
 
     return distances.sort(sort);
 }
 
+/**
+ * Set languages on `node` from multiple
+ * language--distance tuples.
+ *
+ * @param {Node} node
+ * @param {Array.<Array.<string, number>>} languages
+ */
+
 function setLanguages(node, languages) {
-    var primaryLanguage = languages[0][0];
+    var primaryLanguage;
+
+    primaryLanguage = languages[0][0];
 
     node.data.language = primaryLanguage === 'und' ? null : primaryLanguage;
     node.data.languages = languages;
 }
 
+/**
+ * Handler for a change inside a parent.
+ *
+ * @param {Parent} parent
+ */
+
 function onchangeinparent(parent) {
-    var node, languageObject, languages, iterator, length, language;
+    var node,
+        dictionary,
+        languages,
+        tuple,
+        index;
 
     if (!parent) {
         return;
     }
 
+    dictionary = {};
+
     node = parent.head;
-    languageObject = {};
 
     while (node) {
         languages = node.data.languages;
 
         if (languages) {
-            iterator = -1;
-            length = languages.length;
+            index = languages.length;
 
-            while (++iterator < length) {
-                language = languages[iterator];
+            while (index--) {
+                tuple = languages[index];
 
-                if (language[0] in languageObject) {
-                    languageObject[language[0]] += language[1];
+                if (tuple[0] in dictionary) {
+                    dictionary[tuple[0]] += tuple[1];
                 } else {
-                    languageObject[language[0]] = language[1];
+                    dictionary[tuple[0]] = tuple[1];
                 }
             }
         }
@@ -64,31 +108,63 @@ function onchangeinparent(parent) {
         node = node.next;
     }
 
-    setLanguages(parent, sortDistanceObject(languageObject));
+    setLanguages(parent, sortDistanceObject(dictionary));
 
     onchangeinparent(parent.parent);
 }
 
-function onchange() {
-    setLanguages(this, franc.all(this.toString()));
+/**
+ * Handler for a change inside a `SentenceNode`.
+ *
+ * @this {SentenceNode}
+ */
 
-    onchangeinparent(this.parent);
+function onchange() {
+    var self;
+
+    self = this;
+
+    setLanguages(self, franc.all(self.toString()));
+
+    onchangeinparent(self.parent);
 }
 
-function plugin(tree) {
+/**
+ * Define `language`.
+ *
+ * @param {Node} tree
+ */
+
+function language(tree) {
     tree.visitType(tree.PARAGRAPH_NODE, onchangeinparent);
 }
 
-exports = module.exports = plugin;
+/**
+ * Define `attach`.
+ *
+ * @param {Retext} retext
+ */
 
 function attach(retext) {
-    var TextOM = retext.parser.TextOM;
+    var SentenceNode;
 
     retext.use(visit);
 
-    TextOM.SentenceNode.on('changetextinside', onchange);
-    TextOM.SentenceNode.on('removeinside', onchange);
-    TextOM.SentenceNode.on('insertinside', onchange);
+    SentenceNode = retext.parser.TextOM.SentenceNode;
+
+    SentenceNode.on('changetextinside', onchange);
+    SentenceNode.on('removeinside', onchange);
+    SentenceNode.on('insertinside', onchange);
 }
 
-exports.attach = attach;
+/**
+ * Expose `attach`.
+ */
+
+language.attach = attach;
+
+/**
+ * Expose `language`.
+ */
+
+module.exports = language;
